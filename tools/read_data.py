9cf8bc06776451a524e34152a071632ed445e41b
Ilya Makarov <ilya.makarov@behavox.com>
2019-11-27 15:35:19 +0000

    Added HardwareID decode; Refactoring

--- tools/read_data.py
+++ tools/read_data.py
@@ -1,56 +1,20 @@
 #!/usr/bin/env python
-
 import sys
 import argparse
 import logging
 import struct
 
 import serial
+from enum import Enum
 
 from msp_codes import MspCodes
+from crsf_crc import CrsfCrc, calc_crc
 from crsf_codes import CrsfFrameAddress, CrsfFrameType, CrsfCommandID,\
-    CrsfDataType, CrsfVtxXPower, CrsfVtxPitmode, CrsfVtxInterface
+    CrsfDataType, CrsfVtxXPower, CrsfVtxPitmode, CrsfVtxInterface, CrsfHardwareID
+
 
 SYNC_BYTE = 0xC8
 MAX_FRAME_SYZE = 62
-
-crc8tab = [
-    0x00, 0xD5, 0x7F, 0xAA, 0xFE, 0x2B, 0x81, 0x54, 0x29, 0xFC, 0x56, 0x83, 0xD7, 0x02, 0xA8, 0x7D,
-    0x52, 0x87, 0x2D, 0xF8, 0xAC, 0x79, 0xD3, 0x06, 0x7B, 0xAE, 0x04, 0xD1, 0x85, 0x50, 0xFA, 0x2F,
-    0xA4, 0x71, 0xDB, 0x0E, 0x5A, 0x8F, 0x25, 0xF0, 0x8D, 0x58, 0xF2, 0x27, 0x73, 0xA6, 0x0C, 0xD9,
-    0xF6, 0x23, 0x89, 0x5C, 0x08, 0xDD, 0x77, 0xA2, 0xDF, 0x0A, 0xA0, 0x75, 0x21, 0xF4, 0x5E, 0x8B,
-    0x9D, 0x48, 0xE2, 0x37, 0x63, 0xB6, 0x1C, 0xC9, 0xB4, 0x61, 0xCB, 0x1E, 0x4A, 0x9F, 0x35, 0xE0,
-    0xCF, 0x1A, 0xB0, 0x65, 0x31, 0xE4, 0x4E, 0x9B, 0xE6, 0x33, 0x99, 0x4C, 0x18, 0xCD, 0x67, 0xB2,
-    0x39, 0xEC, 0x46, 0x93, 0xC7, 0x12, 0xB8, 0x6D, 0x10, 0xC5, 0x6F, 0xBA, 0xEE, 0x3B, 0x91, 0x44,
-    0x6B, 0xBE, 0x14, 0xC1, 0x95, 0x40, 0xEA, 0x3F, 0x42, 0x97, 0x3D, 0xE8, 0xBC, 0x69, 0xC3, 0x16,
-    0xEF, 0x3A, 0x90, 0x45, 0x11, 0xC4, 0x6E, 0xBB, 0xC6, 0x13, 0xB9, 0x6C, 0x38, 0xED, 0x47, 0x92,
-    0xBD, 0x68, 0xC2, 0x17, 0x43, 0x96, 0x3C, 0xE9, 0x94, 0x41, 0xEB, 0x3E, 0x6A, 0xBF, 0x15, 0xC0,
-    0x4B, 0x9E, 0x34, 0xE1, 0xB5, 0x60, 0xCA, 0x1F, 0x62, 0xB7, 0x1D, 0xC8, 0x9C, 0x49, 0xE3, 0x36,
-    0x19, 0xCC, 0x66, 0xB3, 0xE7, 0x32, 0x98, 0x4D, 0x30, 0xE5, 0x4F, 0x9A, 0xCE, 0x1B, 0xB1, 0x64,
-    0x72, 0xA7, 0x0D, 0xD8, 0x8C, 0x59, 0xF3, 0x26, 0x5B, 0x8E, 0x24, 0xF1, 0xA5, 0x70, 0xDA, 0x0F,
-    0x20, 0xF5, 0x5F, 0x8A, 0xDE, 0x0B, 0xA1, 0x74, 0x09, 0xDC, 0x76, 0xA3, 0xF7, 0x22, 0x88, 0x5D,
-    0xD6, 0x03, 0xA9, 0x7C, 0x28, 0xFD, 0x57, 0x82, 0xFF, 0x2A, 0x80, 0x55, 0x01, 0xD4, 0x7E, 0xAB,
-    0x84, 0x51, 0xFB, 0x2E, 0x7A, 0xAF, 0x05, 0xD0, 0xAD, 0x78, 0xD2, 0x07, 0x53, 0x86, 0x2C, 0xF9
-]
-
-crc8tabCmd = [
-    0x00, 0xBA, 0xCE, 0x74, 0x26, 0x9C, 0xE8, 0x52, 0x4C, 0xF6, 0x82, 0x38, 0x6A, 0xD0, 0xA4, 0x1E,
-    0x98, 0x22, 0x56, 0xEC, 0xBE, 0x04, 0x70, 0xCA, 0xD4, 0x6E, 0x1A, 0xA0, 0xF2, 0x48, 0x3C, 0x86,
-    0x8A, 0x30, 0x44, 0xFE, 0xAC, 0x16, 0x62, 0xD8, 0xC6, 0x7C, 0x08, 0xB2, 0xE0, 0x5A, 0x2E, 0x94,
-    0x12, 0xA8, 0xDC, 0x66, 0x34, 0x8E, 0xFA, 0x40, 0x5E, 0xE4, 0x90, 0x2A, 0x78, 0xC2, 0xB6, 0x0C,
-    0xAE, 0x14, 0x60, 0xDA, 0x88, 0x32, 0x46, 0xFC, 0xE2, 0x58, 0x2C, 0x96, 0xC4, 0x7E, 0x0A, 0xB0,
-    0x36, 0x8C, 0xF8, 0x42, 0x10, 0xAA, 0xDE, 0x64, 0x7A, 0xC0, 0xB4, 0x0E, 0x5C, 0xE6, 0x92, 0x28,
-    0x24, 0x9E, 0xEA, 0x50, 0x02, 0xB8, 0xCC, 0x76, 0x68, 0xD2, 0xA6, 0x1C, 0x4E, 0xF4, 0x80, 0x3A,
-    0xBC, 0x06, 0x72, 0xC8, 0x9A, 0x20, 0x54, 0xEE, 0xF0, 0x4A, 0x3E, 0x84, 0xD6, 0x6C, 0x18, 0xA2,
-    0xE6, 0x5C, 0x28, 0x92, 0xC0, 0x7A, 0x0E, 0xB4, 0xAA, 0x10, 0x64, 0xDE, 0x8C, 0x36, 0x42, 0xF8,
-    0x7E, 0xC4, 0xB0, 0x0A, 0x58, 0xE2, 0x96, 0x2C, 0x32, 0x88, 0xFC, 0x46, 0x14, 0xAE, 0xDA, 0x60,
-    0x6C, 0xD6, 0xA2, 0x18, 0x4A, 0xF0, 0x84, 0x3E, 0x20, 0x9A, 0xEE, 0x54, 0x06, 0xBC, 0xC8, 0x72,
-    0xF4, 0x4E, 0x3A, 0x80, 0xD2, 0x68, 0x1C, 0xA6, 0xB8, 0x02, 0x76, 0xCC, 0x9E, 0x24, 0x50, 0xEA,
-    0x48, 0xF2, 0x86, 0x3C, 0x6E, 0xD4, 0xA0, 0x1A, 0x04, 0xBE, 0xCA, 0x70, 0x22, 0x98, 0xEC, 0x56,
-    0xD0, 0x6A, 0x1E, 0xA4, 0xF6, 0x4C, 0x38, 0x82, 0x9C, 0x26, 0x52, 0xE8, 0xBA, 0x00, 0x74, 0xCE,
-    0xC2, 0x78, 0x0C, 0xB6, 0xE4, 0x5E, 0x2A, 0x90, 0x8E, 0x34, 0x40, 0xFA, 0xA8, 0x12, 0x66, 0xDC,
-    0x5A, 0xE0, 0x94, 0x2E, 0x7C, 0xC6, 0xB2, 0x08, 0x16, 0xAC, 0xD8, 0x62, 0x30, 0x8A, 0xFE, 0x44
-]
 
 
 def unpack_sting(bytes_list):
@@ -95,34 +59,6 @@ def bytes_to_int_list(bytes):
     return result
 
 
-def calc_crc(buf, tab='def'):
-    length = len(buf)
-    crc = 0
-    for i in range(length):
-        if tab == 'def':
-            crc = crc8tab[crc ^ int(ord(buf[i]))]
-        elif tab == 'cmd':
-            crc = crc8tabCmd[crc ^ int(ord(buf[i]))]
-    return crc
-
-
-class CrsfCrc(object):
-    def __init__(self, crc, buf):
-        self.buf = buf
-        self.crc = crc
-        self.calculated_crc = calc_crc(buf)
-
-    def __str__(self):
-        if self.crc == self.calculated_crc:
-            return "OK"
-        else:
-            LOG.error("Wrong CRC: {} != {}".format(self.crc, self.calculated_crc))
-            return "ERROR"
-
-    def verify(self):
-        return self.crc == self.calculated_crc
-
-
 class CrsfPayload(object):
     payload = None
 
@@ -145,26 +81,13 @@ class CrsfPayload(object):
         )
 
     @staticmethod
-    def decode_device_ping(payload):
+    def decode_battery_sensor(payload):
         return (
             ("raw", bytes_to_int_list(payload)),
-            ("dst address", CrsfFrameAddress(ord(payload[0]))),
-            ("src address", CrsfFrameAddress(ord(payload[1]))),
-        )
-
-    @staticmethod
-    def decode_device_info(payload):
-        return (
-            ("raw", bytes_to_int_list(payload)),
-            ("dst address", CrsfFrameAddress(ord(payload[0]))),
-            ("src address", CrsfFrameAddress(ord(payload[1]))),
-            ("name", "".join(payload[2:-15])),
-            # Zero byte
-            ("serial number", bytes_to_uint(payload[-14:-10])),
-            ("hardware id", hex(bytes_to_uint(payload[-10:-6]))),  # CrsfHardwareID
-            ("firmware id", bytes_to_uint(payload[-6:-2])),
-            ("parameter count", ord(payload[-2])),
-            ("device info version", ord(payload[-1]))
+            ("voltage", float((ord(payload[0]) << 8) + ord(payload[1])) / 10),
+            ("current", float((ord(payload[2]) << 8) + ord(payload[3])) / 10),
+            ("consumption", (ord(payload[4]) << 16) + (ord(payload[5]) << 8) + ord(payload[6])),
+            ("battary procentage", bytes_to_uint(payload[7])),
         )
 
     @staticmethod
@@ -210,23 +133,27 @@ class CrsfPayload(object):
         )
 
     @staticmethod
-    def decode_parameter_write(payload):
+    def decode_device_ping(payload):
         return (
             ("raw", bytes_to_int_list(payload)),
             ("dst address", CrsfFrameAddress(ord(payload[0]))),
             ("src address", CrsfFrameAddress(ord(payload[1]))),
-            ("parameter number", ord(payload[2])),
-            ("data", bytes_to_int_list(payload[3:]))
         )
 
     @staticmethod
-    def decode_parameter_read(payload):
+    def decode_device_info(payload):
         return (
             ("raw", bytes_to_int_list(payload)),
             ("dst address", CrsfFrameAddress(ord(payload[0]))),
             ("src address", CrsfFrameAddress(ord(payload[1]))),
-            ("parameter number", ord(payload[2])),
-            ("parameter chunk number", ord(payload[3]))
+            ("name", "".join(payload[2:-15])),
+            # Zero byte
+            ("serial number", bytes_to_uint(payload[-14:-10])),
+            ("hardware id", hex(bytes_to_uint(payload[-10:-6]))),  # CrsfHardwareID
+            ("hardware id (decoded)", CrsfHardwareID(bytes_to_uint(payload[-10:-6]) >> 3 << 3)),
+            ("firmware id", bytes_to_uint(payload[-6:-2])),
+            ("parameter count", ord(payload[-2])),
+            ("device info version", ord(payload[-1]))
         )
 
     # FIXME
@@ -240,8 +167,40 @@ class CrsfPayload(object):
             ("parameter chunks", ord(payload[3])),
             ("parent folder", ord(payload[4])),
             # ("hidden", bytes_to_uint(payload[5]) >> 7),  # TODO: test with hidden fields
-            ("data type", CrsfDataType(ord(payload[5])).name),
+            ("data type", CrsfDataType(ord(payload[5]))),
             ("payload", unpack_sting(payload[6:]))  # TODO: Unpack more data
+        )
+
+    @staticmethod
+    def decode_parameter_read(payload):
+        return (
+            ("raw", bytes_to_int_list(payload)),
+            ("dst address", CrsfFrameAddress(ord(payload[0]))),
+            ("src address", CrsfFrameAddress(ord(payload[1]))),
+            ("parameter number", ord(payload[2])),
+            ("parameter chunk number", ord(payload[3]))
+        )
+
+    @staticmethod
+    def decode_parameter_write(payload):
+        return (
+            ("raw", bytes_to_int_list(payload)),
+            ("dst address", CrsfFrameAddress(ord(payload[0]))),
+            ("src address", CrsfFrameAddress(ord(payload[1]))),
+            ("parameter number", ord(payload[2])),
+            ("data", bytes_to_int_list(payload[3:]))
+        )
+
+    @staticmethod
+    def decode_command(payload):
+        return (
+            ("raw", bytes_to_int_list(payload)),
+            ("dst address", CrsfFrameAddress(ord(payload[0]))),
+            ("src address", CrsfFrameAddress(ord(payload[1]))),
+            ("command id", CrsfCommandID(ord(payload[2]))),
+            ("command payload", bytes_to_int_list(payload[3:-1])),
+            ("command crc", ord(payload[-1])),
+            ("calculated crc", calc_crc(list_to_bytes([0x32] + bytes_to_int_list(payload[0:-1])), 'cmd'))  # FIXME
         )
 
     @staticmethod
@@ -273,19 +232,6 @@ class CrsfPayload(object):
         )
 
     @staticmethod
-    def decode_command(payload):
-        return (
-            ("raw", bytes_to_int_list(payload)),
-            ("dst address", CrsfFrameAddress(ord(payload[0]))),
-            ("src address", CrsfFrameAddress(ord(payload[1]))),
-            ("command id", CrsfCommandID(ord(payload[2])).value),
-            ("command payload", bytes_to_int_list(payload[3:-1])),
-            ("command crc", ord(payload[-1])),
-            ("calculated crc",
-             calc_crc(list_to_bytes([CrsfFrameType.COMMAND.value] + bytes_to_int_list(payload[0:-1])), 'cmd'))  # FIXME
-        )
-
-    @staticmethod
     def decode_msp_write(payload):
         return (
             ("raw", bytes_to_int_list(payload)),
@@ -306,16 +252,6 @@ class CrsfPayload(object):
             ("pitch", float((ord(payload[0]) << 8) + ord(payload[1])) / 1000),
             ("roll", float((ord(payload[2]) << 8) + ord(payload[3])) / 1000),
             ("yaw", float((ord(payload[4]) << 8) + ord(payload[5])) / 1000)
-        )
-
-    @staticmethod
-    def decode_battery_sensor(payload):
-        return (
-            ("raw", bytes_to_int_list(payload)),
-            ("voltage", float((ord(payload[0]) << 8) + ord(payload[1])) / 10),
-            ("current", float((ord(payload[2]) << 8) + ord(payload[3])) / 10),
-            ("consumption", (ord(payload[4]) << 16) + (ord(payload[5]) << 8) + ord(payload[6])),
-            ("battary procentage", bytes_to_uint(payload[7])),
         )
 
     @staticmethod
@@ -438,6 +374,7 @@ class Reader(object):
     def read_frame(self, address=None):
         self.frames_total += 1
         LOG.debug("%s - Reading frame" % self.frames_total)
+
         buf = [address]
         buf += self.read_data(1)  # length
         if ord(buf[1]) > MAX_FRAME_SYZE:
@@ -445,7 +382,7 @@ class Reader(object):
 
         buf += self.read_data(1)  # type
         buf += self.read_data(ord(buf[1]) - 2)  # payload
-        buf += self.read_data(1)
+        buf += self.read_data(1)  # crc
 
         try:
             frame = CrsfFrame()
@@ -489,6 +426,24 @@ def parse_args():
     return parser.parse_args()
 
 
+def print_frame(frame):
+    LOG.info("==========New frame===========")
+
+    for field in frame.fields:
+        if hasattr(field[1], 'payload'):
+            LOG.info("payload:")
+            for j in getattr(field[1], 'payload'):
+                name = j[0]
+                value = j[1]
+                if isinstance(value, Enum) and hasattr(value, 'name'):
+                    value = getattr(value, 'name')
+                LOG.info("  %s: %s" % (name, value))
+        elif hasattr(field[1], 'name'):
+            LOG.info("%s: %s" % (field[0], field[1].name))
+        else:
+            LOG.info("%s: %s" % (field[0], field[1]))
+
+
 if __name__ == "__main__":
     args = parse_args()
     LOG = setup_logging()
@@ -515,7 +470,7 @@ if __name__ == "__main__":
             if byte == "":
                 break
             # elif bytes_to_uint(byte) in [CrsfFrameAddress(e).value for e in CrsfFrameAddress.__members__.values()]:
-            elif bytes_to_uint(byte) == SYNC_BYTE:
+            elif ord(byte) == SYNC_BYTE:
                 frame = reader.read_frame(address=byte)
                 if frame is None:
                     continue
@@ -525,16 +480,7 @@ if __name__ == "__main__":
                     continue
 
                 if args.extended_view:
-                    LOG.info("==========New frame===========")
-                    for field in frame.fields:
-                        if hasattr(field[1], 'payload'):
-                            LOG.info("payload:")
-                            for j in getattr(field[1], 'payload'):
-                                LOG.info("  %s: %s" % (j[0], j[1]))
-                        elif hasattr(field[1], 'name'):
-                            LOG.info("%s: %s" % (field[0], field[1].name))
-                        else:
-                            LOG.info("%s: %s" % (field[0], field[1]))
+                    print_frame(frame)
                 else:
                     LOG.info(frame)
             else:
